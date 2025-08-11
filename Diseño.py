60e18962e43bb6f132325357aee4b2fde0c29838
Emellie29 <emellie.lopez@udeo.edu.gt>
2025-08-11 11:32:31 -0600

    primera parte

--- Diseño.py
+++ Diseño.py
@@ -2,9 +2,9 @@ registro={}
 while True:
     print("••••••Menú Principal••••••")
     print("1. Registrar pacientes")
-    print("2. Buscar pacientes")
-    print("3. Eleminar pacientes")
-    print("4. ")
+    print("2. Mostrar pacientes")
+    print("3. Buscar pacientes")
+    print("4. Eleminar pacientes")
     print("5. Salir")
     opcion=int("Seleccione una opcion: ")
     match opcion:
@@ -15,4 +15,8 @@ while True:
                 print(f"\nEstudiante No.{i + 1}")
                 carnet = int(input("Ingrese codigo de paciente: "))
                 nombre = input("Ingrese el nombre completo del paciente: ")
-                edad=int("Ingrese la fecha de nacimiento del paciente (03/05/1919): ")
+                edad=input("Ingrese la edad del paciente (18 años): ")
+                registro[carnet]={
+                    "nombre":nombre,
+                    "edad":edad
+                }
