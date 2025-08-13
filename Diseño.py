b30f2f6cc6cce3ff8e9b893c4110119783690ac9
Emellie29 <emellie.lopez@udeo.edu.gt>
2025-08-12 21:03:09 -0600

    Simplificacion terminada

--- Diseño.py
+++ Diseño.py
@@ -1,4 +1,59 @@
-registro={}
+pacientes={}
+def registrar():
+    print("Registrar pacientes.")
+    codigopaciente=input("Código del paciente (o 'regresar'): ")
+    if codigopaciente == "regresar":
+        return
+    if codigopaciente in pacientes:
+        print("Ese código ya esta registrado.")
+        return
+    nombre=input("Nombre completo del paciente: ")
+    edad=input("Edad (ej. 28 años): ")
+    pacientes[codigopaciente]={
+        "nombre":nombre,
+        "edad":edad
+    }
+    print("El paciente fue registrado correctamente.")
+def mostrar():
+    print("Mostrar Pacientes")
+    if not pacientes:
+        print("No hay pacientes registrados.")
+    else:
+        for codigopaciente,datos in pacientes.items():
+            print(f"{datos['nombre']} - Edad: {datos['edad']} - Codigo: {codigopaciente}")
+    input("\nPresione la tecla enter para regresar al menú.")
+def buscar():
+    print("Buscar paciente")
+    nombre=input("Nombre completo del paciente (o 'regresar'): ")
+    if nombre=='regresar':
+        return
+    encontrado=False
+    for codigopaciente,datos in pacientes.items():
+        if nombre in datos['nombre']:
+            print(f"{datos['nombre']} - Edad: {datos['edad']} - Codigo: {codigopaciente}")
+            encontrado=True
+    if not encontrado:
+        print("No se encontró al paciente.")
+    input("\nPresione la tecla enter para regresar al menú.")
+def eliminar():
+    print("Eliminar Paciente")
+    codigopaciente=input("Código a eliminar (o 'regresar'): ")
+    if codigopaciente == "regresar":
+        return
+    if codigopaciente not in pacientes:
+        print("Codigo no encontrado.")
+        return
+    print(f"Paciente encontrado {pacientes[codigopaciente]['nombre']} - Edad: {pacientes[codigopaciente]['edad']}.")
+    confirmacion1= input("¿Esta seguro de querer eliminar al paciente? (Sí/No): ")
+    if confirmacion1 != "Si":
+        print("Operación cancelada.")
+        return
+    confirmacion2=input("Confirme nuevamente para eliminar al paciente (Sí/No): ")
+    if confirmacion2 == "Si":
+        del pacientes[codigopaciente]
+        print("Se elimino correctamente al paciente.")
+    else:
+        print("Operación cancelada.")
 while True:
     print("••••••Menú Principal••••••")
     print("1. Registrar pacientes")
@@ -6,23 +61,18 @@ while True:
     print("3. Buscar pacientes")
     print("4. Eleminar pacientes")
     print("5. Salir")
-    opcion=int("Seleccione una opcion: ")
+    opcion=input("Seleccione una opcion (1 a 5): ")
     match opcion:
         case "1":
-            print("Registrar pacientes")
-            cantidad = int(input("\nIngrese la cantidad de pacientes: "))
-            for i in range(cantidad):
-                print(f"\nEstudiante No.{i + 1}")
-                codigo = int(input("Ingrese codigo de paciente: "))
-                nombre = input("Ingrese el nombre completo del paciente: ")
-                edad=input("Ingrese la edad del paciente (18 años): ")
-                registro[codigo]={
-                    "nombre":nombre,
-                    "edad":edad
-                }
+            registrar()
         case "2":
-            print("Mostrar pacientes")
-            if not registro:
-                print("No se ha podido registrar pacientes")
-            else:
-                for codigo
+            mostrar()
+        case "3":
+            buscar()
+        case "4":
+            eliminar()
+        case "5":
+            print("Saliendo del sistema de registro.")
+            break
+        case _:
+            print("Opcion no valida.")
